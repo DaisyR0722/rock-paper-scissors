00ae2bd33a1b3dd89b7f05d9c8977793021a8318
Daisy <156558580+DaisyR0722@users.noreply.github.com>
2024-08-16 19:33:17 -0500

    Add human choice function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,13 @@
 function getComputerChoice(){
-    choices = ["rock","paper","scissors"];
-    randomChoice = choices[Math.floor(Math.random() * choices.length)];
+   let choices = ["rock","paper","scissors"];
+   let randomChoice = choices[Math.floor(Math.random() * choices.length)];
     return randomChoice;
 }
 console.log(getComputerChoice());
+
+function getHumanChoice(){
+   let options = prompt("Choose your weapon","rock","paper","scissors");
+    return options;
+}
+console.log(getHumanChoice());
+
