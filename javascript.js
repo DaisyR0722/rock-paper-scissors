c82453d0fc6d758a718f0d2cfcf6ff135772dad2
Daisy <156558580+DaisyR0722@users.noreply.github.com>
2024-08-16 20:13:50 -0500

    Add play round function
    Depending on players choices, the round winner’s score increments and logs a winner announcement.

--- javascript.js
+++ javascript.js
@@ -6,8 +6,24 @@ function getComputerChoice(){
 console.log(getComputerChoice());
 
 function getHumanChoice(){
-   let options = prompt("Choose your weapon","rock","paper","scissors");
+   let options = prompt("Choose your weapon","rock","paper","scissors").toLowerCase();
     return options;
 }
 console.log(getHumanChoice());
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+  if (humanChoice === computerChoice) {
+        return "It's a tie!";
+    } else if (humanChoice === "rock" && computerChoice === "paper"){
+        computerScore++
+        return "You lose!Paper beats rock!";
+    }
+}
+
+console.log(playRound());
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
