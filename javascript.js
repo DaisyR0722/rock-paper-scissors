8b130029e7e7b83f8b43bed9f239d32e97db8f17
Daisy <156558580+DaisyR0722@users.noreply.github.com>
2026-06-03 15:15:21 -0500

    Add interactive buttons and JavaScript

--- javascript.js
+++ javascript.js
@@ -1,65 +1,94 @@
 //Consists computer's random choices 
+const rockButton = document.querySelector(".rock")
+const paperButton = document.querySelector(".paper")
+const scissorsButton = document.querySelector(".scissors")
+const resultDiv = document.querySelector(".result")
+const runningScoreDiv = document.querySelector(".running-score");
+const humanScoreSpan = document.querySelector(".human-score");
+const computerScoreSpan = document.querySelector(".computer-score");
+
 function getComputerChoice(){
-   const choices = ["rock","paper","scissors"];
-   const randomChoice = choices[Math.floor(Math.random() * choices.length)];
-    return randomChoice;
+    let choices = ["rock","paper","scissors"];
+    let randomChoice = Math.floor(Math.random()*choices.length);
+    let computerChoice = choices[randomChoice];
+    return computerChoice;
+}
+/*
+function getHumanChoice(){  
+    let humanChoice = prompt("What's your choice?", "rock", "paper", "scissors").toLowerCase();
+    if(["rock","paper", "scissors"].includes(humanChoice)){
+        return humanChoice;
+    } 
+}
+*/
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+   if (humanChoice === computerChoice) {
+    const paragraph = document.createElement('p');
+    paragraph.innerText = "It's a tie!"
+    resultDiv.appendChild(paragraph)
+     } else if 
+      (humanChoice === "rock" && computerChoice === "paper" 
+        || humanChoice === "paper" && computerChoice === "scissors" 
+        || humanChoice === "scissors" && computerChoice === "rock"){
+      computerScore++
+      const paragraph = document.createElement('p');
+      paragraph.innerText = `You lose! ${computerChoice} beats ${humanChoice}!`
+      resultDiv.appendChild(paragraph)
+    } else if 
+        (humanChoice === "rock" && computerChoice === "scissors" 
+        || humanChoice === "paper" && computerChoice === "rock" 
+        || humanChoice === "scissors" && computerChoice === "paper"){
+      humanScore++
+      const paragraph = document.createElement('p');
+      paragraph.innerText = `You win! ${humanChoice} beats ${computerChoice}!`
+      resultDiv.appendChild(paragraph)
+    } 
+
 }
 
-//Consisrs of human options to write
-function getHumanChoice(){
-    const options = prompt("Choose your weapon","rock","paper","scissors").toLowerCase();
-    return options;
+const checkForWinner = (humanScore,computerScore) => {
+    if(humanScore === 5){
+      const heading = document.createElement('h2');
+      heading.innerText = `You won ${humanScore} to ${computerScore}. Congratulations on beating the game!`
+      resultDiv.appendChild(heading)
+    } 
+    if (computerScore === 5){
+      const heading = document.createElement('h2');
+      heading.innerText = `You lost ${computerScore} to ${humanScore}. Try again!`
+      resultDiv.appendChild(heading)
+    } 
 }
 
-//Plays one round with player choices
-function playRound(humanSelection, computerSelection){ 
-    if (humanSelection === computerSelection) {
-      return "It's a tie!";
-     } else if (humanSelection === "rock" && computerSelection === "paper"){
-      return "You lose!Paper beats rock!";
-    } else if (humanSelection === "paper" && computerSelection === "scissors"){
-      return "You lose! Scissors beats paper!";
-     } else if (humanSelection === "scissors" && computerSelection === "rock"){
-      return "You lose! Rock beats scissors!";
-    } else if (humanSelection === "rock" && computerSelection === "scissors"){
-      return "You win! Rock beats scissors!";
-     } else if  (humanSelection === "paper" && computerSelection === "rock"){
-      return "You win! Paper beats rock!";
-     } else if (humanSelection === "scissors" && computerSelection === "paper"){
-      return "You win! Scissors beats paper!";
-    }
-  } 
-const humanSelection = getHumanChoice();
-console.log(humanSelection);
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-console.log(playRound(humanSelection,computerSelection));
+const showRunningScore = (humanScore, computerScore) =>{
+  humanScoreSpan.innerText = `Player Score: ${humanScore}`;
+  computerScoreSpan.innerText = `Computer Score: ${computerScore}`;
 
-//Plays games and tracks score
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-    //Loops game five times
-    for (i = 0; i < 5; i++){
-        const result = playRound(getHumanChoice(), getComputerChoice());
-        console.log(result);
-        if (result.includes("win")) {
-            humanScore++;
-            console.log(`computer: ${computerScore} | human: ${humanScore}`);
-          } else if (result.includes("lose")) {
-            computerScore++;
-            console.log(`computer: ${computerScore} | human: ${humanScore}`);
-          }
-}
-    //Determines winner with score
-    console.log("Final results: Human " + humanScore + " Computer: " + computerScore);
-        if (humanScore > computerScore){
-            console.log("You win the game!");
-        } else if (humanScore < computerScore){
-            console.log("You lose the game!");
-        } else {
-            console.log("The game is a tie!");
-        }   
 }
 
-playGame();
+rockButton.addEventListener("click", ()=>{
+  const computerChoice = getComputerChoice();
+  const humanChoice = 'rock';
+  playRound(humanChoice,computerChoice);
+  checkForWinner(humanScore, computerScore);
+  showRunningScore(humanScore, computerScore);
+})
+paperButton.addEventListener("click", ()=>{
+  const computerChoice = getComputerChoice();
+  const humanChoice = 'paper';
+  playRound(humanChoice,computerChoice);
+  checkForWinner(humanScore, computerScore);
+  showRunningScore(humanScore, computerScore);
+})
+scissorsButton.addEventListener("click", ()=>{
+  const computerChoice = getComputerChoice();
+  const humanChoice = 'scissors';
+  playRound(humanChoice,computerChoice);
+  checkForWinner(humanScore, computerScore);
+  showRunningScore(humanScore, computerScore);
+})
+
+
+
